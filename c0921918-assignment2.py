f396569a958ea7cb83344dfbd8c2c51fca769e72
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2023-12-02 01:48:44 -0500

    Add files via upload
    Basic program that ask user input and prints the output.

---
+++ c0921918-assignment2.py
@@ -0,0 +1,77 @@
+#CSD-1233-01
+#Assignment 2
+#Romika Chaudhary
+#Sept 20, 2023
+
+print("Answer to multiple choice review questions 1 to 13")
+#Answer to question 1
+print("Question 1: c")
+
+#Answer to question 2
+print("Question 2: b")
+
+#Answer to question 3
+print("Question 3: d")
+
+#Answer to question 4
+print("Question 4: b")
+
+#Answer to question 5
+print("Question 5: a")
+
+#Answer to question 6
+print("Question 6: c")
+
+#Answer to question 7
+print("Question 7: a")
+
+#Answer to question 8
+print("Question 8: b")
+
+#Answer to question 9
+print("Question 9: d")
+
+#Answer to question 10
+print("Question 10: a")
+
+#Answer to question 11
+print("Question 11: b")
+
+#Answer to question 12
+print("Question 12: d")
+
+#Answer to question 13
+print("Question 13: b")
+
+print("\r")
+
+print("Answer to True or False questions 3 and 4")
+#Answer to question 3
+print("Question 3: False")
+
+#Answer to question 4
+print("Question 4: True")
+
+print("\r")
+
+print('Programming Assignments')
+#Code to question 1
+print("Answer to question 1")
+user_name = input('Please enter your name: ')
+user_favColour = input('Please enter your favourite colour: ')
+print("Your name is " +user_name, "and your favorite color is " +user_favColour+".")
+print("\r")
+
+#Code to question 2
+print("Answer to question 2")
+first_name = input('Please enter your first name or initial name: ')
+last_name = input('Please enter your last name: ')
+street_num = input('Please enter your street number: ')
+street_name = input('Please enter your street name: ')
+street_type = input('Please enter your street type: ')
+city = input('Please enter your city: ')
+province = input('Please enter your province: ')
+postal_code = input('Please enter your postal code: ')
+print(first_name, last_name)
+print(street_num, street_name, street_type)
+print(city, province, postal_code)
